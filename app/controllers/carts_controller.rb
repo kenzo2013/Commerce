9dd64daf6f89093f302002587032acd0f708f5f0
kenzo2013 <kkenzo2007@yahoo.fr>
2015-07-31 14:12:19 +0200

    Ajout d'une touche d'Ajax

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -54,11 +54,10 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    debugger
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to store_path, notice: 'You cart is currently empty.' }
+      format.html { redirect_to store_url}
       format.json { head :no_content }
     end
   end
