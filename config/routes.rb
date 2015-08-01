480027f7b4c9d3ad9602407d5fd2af8750979da5
kenzo2013 <kkenzo2007@yahoo.fr>
2015-08-01 13:31:18 +0200

    Internalisation

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,13 @@ get 'admin' => 'admin#index'
   # first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
-  root 'store#index', as: 'store'
+  scope '(:locale)' do
+    resources :orders 
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
